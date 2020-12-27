7158764b3aaa7907fe66676b26d9460735feffa4
T.H Lin <thlin002@gmail.com>
2020-12-27 21:11:34 +0800

    from ubuntu

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
 from fsm import TocMachine
-from utils import send_text_message
+from utils import send_text_message, send_image_url
 
 load_dotenv()
 
