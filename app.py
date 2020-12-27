6a818f2cda4c2fb95d458a484a7a00ceda157ee3
T.H Lin <thlin002@gmail.com>
2020-12-27 15:54:48 +0800

    imgur

--- app.py
+++ app.py
@@ -127,8 +127,8 @@ def webhook_handler():
 
 @app.route("/show-fsm", methods=["GET"])
 def show_fsm():
-    machine.get_graph().draw("fsm.jpeg", prog="dot", format="jpeg")
-    return send_file("fsm.jpeg", mimetype="image/jpeg")
+    machine.get_graph().draw("fsm.svg", prog="dot", format="svg")
+    return send_file("fsm.svg", mimetype="image/svg")
 
 
 if __name__ == "__main__":
